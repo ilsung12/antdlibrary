014324ca84a89958399521890de03cc9a1dc877c
coil <75682926+ilsung12@users.noreply.github.com>
2021-10-17 19:14:07 +0900

    📝 Learn : antd - GetStarted

---
+++ antdlibrary/src/components/Antd/AntdExample.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Getstart from './Getstart';
+
+export default function AntdExample() {
+  return (
+    <div>
+      <Getstart />
+    </div>
+  );
+}
---
+++ antdlibrary/src/components/Antd/Getstart.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import { DatePicker, message } from 'antd';
+
+export default function Getstart() {
+  const [date, setDate] = useState(null);
+  const handleChange = (value) => {
+    message.info(
+      `Selected Date: ${value ? value.format('YYYY-MM-DD') : 'None'}`
+    );
+    setDate(value);
+  };
+  return (
+    <div style={{ width: 400, margin: '100px auto' }}>
+      <DatePicker onChange={handleChange} />
+      <div style={{ marginTop: 16 }}>
+        Selected Date: {date ? date.format('YYYY-MM-DD') : 'None'}
+      </div>
+    </div>
+  );
+}
