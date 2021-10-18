e14bd6c5f861c80fd9c02eaf6f12375c72970b64
coil <75682926+ilsung12@users.noreply.github.com>
2021-10-18 22:41:17 +0900

    📝 Learn : antd - 2

--- antdlibrary/src/components/Antd/AntdExample.jsx
+++ antdlibrary/src/components/Antd/AntdExample.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import FormExample from './FormExample';
 import Getstart from './Getstart';
 import IconExample from './IconExample';
+import LayoutExample from './LayoutExample';
+import ModalExample from './ModalExample';
+import TableExample from './TableExample';
 import TypographyExample from './TypographyExample';
 
 export default function AntdExample() {
   return (
     <div>
+      <ModalExample />
+      <TableExample />
+      <FormExample />
+      <LayoutExample />
       <Getstart />
       <IconExample />
       <TypographyExample />
